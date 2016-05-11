a23d1a7cb18a4bc5d1a955713a146d75e1fe9acf
Tuss4 <tuss4dbfn@gmail.com>
2016-05-11 13:46:07 -0500

    writing tests.

--- elwrapper/eswrap/tests.py
+++ elwrapper/eswrap/tests.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
-from .queries import CREATE_TEST_TABLE, DROP_TEST_TABLE
 from . import db_conn
+from .queries import CREATE_TEST_TABLE, DROP_TEST_TABLE
+from .writer import Writer
+import os
+import subprocess
 
 
 class ESWrapTest(TestCase):
@@ -14,6 +17,11 @@ class ESWrapTest(TestCase):
     def tearDown(self):
         with self.conn.cursor() as c:
             c.execute(DROP_TEST_TABLE)
+        cmd = "rm {}/eswrap/test_table_es_mapping.py".format(os.getcwd())
+        subprocess.run([cmd], shell=True, check=True)
 
     def test_eswrap(self):
-        print("bruh")
+        w = Writer()
+        w.write_mapping('test_table', 'test_document')
+        files = os.listdir('{}/eswrap'.format(os.getcwd()))
+        self.assertIn('test_table_es_mapping.py', files)
